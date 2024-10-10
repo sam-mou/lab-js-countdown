15ff420015bd4a48f6646d7a36521adf87f42cf8
“Samy <samy.mouahbi@hotmail.com>
2024-10-10 22:36:20 +0200

    solved

--- src/index.js
+++ src/index.js
@@ -8,16 +8,31 @@ let timer = null; // Variable to store the interval
 
 // Your code goes here ...
 
+const startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", startCountdown);
 
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
+  startButton.disabled = true;
 
 
   // Your code goes here ...
-}
+  const newTime = document.getElementById("time");
+ timer = setInterval(() => {
+    remainingTime--;
+    newTime.innerText = remainingTime;
+      if(remainingTime <= 0) {
+        clearInterval(timer);
+        remainingTime = 0;
+        newTime.innerText = remainingTime;
+        showToast();
+      }
+      },1000);
+  }
+
 
 
 
@@ -27,7 +42,12 @@ function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
-
+const toast = document.getElementById("toast");
+toast.classList.add("show");
+setTimeout(() => {
+  toast.classList.remove("show");
+  startButton.disabled = false;
+}, 3000);
 
 
 
